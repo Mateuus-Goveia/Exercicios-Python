0fcf0644d35bd2b2b3a539ddc40eb543e6b69639
Matheus Ferreira Goveia <102265029+Chompbros@users.noreply.github.com>
2022-06-10 22:54:38 -0300

    Include secao 4 folder
    Add news ex files.

--- Secao 4 - Variáveis e Tipos de Dados em Python/ex50.py
+++ Secao 4 - Variáveis e Tipos de Dados em Python/ex50.py
@@ -1,6 +1,10 @@
 """Implemente um programa que calcule o ano de nascimento de uma pessoa a partir de
 sua idade e do ano atual."""
 
-'Desculpa ainda estou resolvendo essa.'
-
+from datetime import date
+a_atual = date.today().year
+print('Descubra o ano do seu nascimento.')
+idade = int(input('Digite sua idade: '))
+a_nasc = a_atual - idade
+print('{} é o ano do seu nascimento.'.format(a_nasc))
 print('FIM')
